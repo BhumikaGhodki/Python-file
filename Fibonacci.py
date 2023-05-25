cbc24d37b9cb889a9359a51e45dc6ec75fa67837
BhumikaGhodki <134388533+BhumikaGhodki@users.noreply.github.com>
2023-05-25 10:46:17 +0530

    Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+def fibo(n): 
+    if n <= 1: 
+        return n 
+    else: 
+        return(fibo(n-1) + fibo(n-2)) 
+  
+ nterms = int(input("Enter number of terms")) 
+  
+  
+ if nterms <= 0: 
+    print("Plese enter a positive integer") 
+ else: 
+    print("Fibonacci sequence:") 
+    for i in range(nterms): 
+        print(fibo(i))
